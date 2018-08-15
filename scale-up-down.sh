44c67d7bf42c711bae1d54485bdf367845951566
Nilesh Suryavanshi <nnilesh7756@gmail.com>
2018-08-15 17:05:23 +0000

    With log file - date -2

--- scale-up-down.sh
+++ scale-up-down.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 
-echo "Please provide count to execute WHILE in loop:"
 
-read num
 
-echo "num 20"
+LOGFILE=/home/ubuntu/nsx-logs/nsx-np/scale-down-"$(date +%Y%d%H%M)".log
+date > $LOGFILE
+num=20
+
 count=1
 
 while [ $count -le $num ]
 do
-	echo "Deleting of App"
-	cf delete -f $1
-	cf delete -f $2
+	echo "Login to org 'test-2'\n" >> $LOGFILE
+	cf login --skip-ssl-validation -a api.system.pcf.local -u admin -p kgZ8E0D-1g2TFuTpMSWmSN6Qlqt4jLEq -o test-2 >> $LOGFILE
+	echo "\n Deleting of App $1 \n" >> $LOGFILE
+	cf delete -f $1 >> $LOGFILE
+	echo "\n Deleting of App $2" >> $LOGFILE
+	cf delete -f $2 >> $LOGFILE
 
-	echo "Pushing new app"
+	echo "\n Pushing new app $1" >> $LOGFILE
 	cd /tmp/sample_app
-	cf push $1
-	cf push $2
+	cf push $1 >> $LOGFILE
+	echo "Pushing new app $2" >> $LOGFILE
+	cf push $2 >> $LOGFILE
 
-	echo "scale of app"
-	cf scale -i 50 $1
-	cf scale -i 50 $2
+	echo "scale of app $1" >> $LOGFILE
+	cf scale -i 10 $1 >> $LOGFILE
+	echo "scale of app $2" >> $LOGFILE
+	cf scale -i 10 $2 >> $LOGFILE
 
+	echo "Sleeping for 15 sec" >> $LOGFILE
 	sleep 15
 	
-	for i in $(seq 0 1 49)
+	for i in $(seq 0 1 9)
 	do
-		echo "Ping to app $1 instance - $i"
+		echo "\n Ping to app $1 instance - $i" >> $LOGFILE
 	
-		cf ssh $1 -i "$i" -c "ping -c 3 66.10.10.10" |  grep 'packet loss' | awk '{print $6 " " $7 " " $8}'
+		cf ssh $1 -i "$i" -c "ping -c 3 66.10.10.10"  >> $LOGFILE
 		
-		echo "Ping to app $2 instance - $i"
-		cf ssh $2 -i "$i" -c "ping -c 3 66.10.10.10" |  grep 'packet loss' | awk '{print $6 " " $7 " " $8}'
+		echo "\n Ping to app $2 instance - $i" >> $LOGFILE
+		cf ssh $2 -i "$i" -c "ping -c 3 66.10.10.10"  >> $LOGFILE
 
 	done
 
